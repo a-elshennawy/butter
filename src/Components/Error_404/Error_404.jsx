22cfd5e862bb2e002c827f7009762534907b5fe9
Shennawy <ahmedshennawy997@gmail.com>
2025-04-06 18:31:21 +0200

    error404

--- src/Components/Error_404/Error_404.jsx
+++ src/Components/Error_404/Error_404.jsx
@@ -13,14 +13,14 @@ export default function Error_404() {
 
       <div className="error">
         <img src="Images/404.webp" alt="" />
-        <div className="content">
-          <i className="fa-solid fa-gears"></i>
-          <h1>Error 404</h1>
-          <h3>
+        <div className="content row">
+          <i className="fa-solid fa-gears col-12"></i>
+          <h1 className="col-12">Error 404</h1>
+          <h3 className="col-12">
             hmmm .. i guess this page is not found or not available at this
             moment
           </h3>
-          <Link className="goHome" to={"/"}>
+          <Link className="goHome col-10" to={"/"}>
             back to home page
           </Link>
         </div>
